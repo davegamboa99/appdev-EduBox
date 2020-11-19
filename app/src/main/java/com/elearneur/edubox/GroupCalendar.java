f6ccacd4d00bc3261dae09e942714b51678a6f86
desierto <monchingdesierto49@gmail.com>
2020-11-19 13:57:53 +0800

    small ui tweak

--- app/src/main/java/com/elearneur/edubox/GroupCalendar.java
+++ app/src/main/java/com/elearneur/edubox/GroupCalendar.java
@@ -10,23 +10,24 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class PersonalCalendar extends AppCompatActivity {
+public class GroupCalendar extends AppCompatActivity {
     private Toolbar toolbar;
-    private FloatingActionButton addevent;
+    private FloatingActionButton addgroupevent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_personal_calendar);
+        setContentView(R.layout.activity_group_calendar);
 
-        toolbar = findViewById(R.id.toolbar_calendar);
+        toolbar = findViewById(R.id.toolbar_group_calendar);
         setSupportActionBar(toolbar);
-        addevent = findViewById(R.id.addevent);
+        addgroupevent = findViewById(R.id.addgroupevent);
 
-        addevent.setOnClickListener(new View.OnClickListener() {
+        addgroupevent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), EventAdd.class);
@@ -38,16 +39,15 @@ public class PersonalCalendar extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.calendar, menu);
+        inflater.inflate(R.menu.group_calendar, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.groups){
-            Intent intent = new Intent(getApplicationContext(), Groups.class);
-            startActivity(intent);
+        if (id == R.id.share){
+            Toast.makeText(getApplicationContext(), "Invitation code copied!", Toast.LENGTH_SHORT).show();
         }
         return true;
     }
--- app/src/main/java/com/elearneur/edubox/Groups.java
+++ app/src/main/java/com/elearneur/edubox/Groups.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Groups extends AppCompatActivity {
     private FloatingActionButton creategroup, joingroup, addgroup;
     private boolean optionsAreVisible = false;
+    private Button group1,group2,group3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,9 @@ public class Groups extends AppCompatActivity {
         creategroup = findViewById(R.id.creategroup);
         joingroup = findViewById(R.id.joingroup);
         addgroup = findViewById(R.id.addgroup);
+        group1 = findViewById(R.id.group1);
+        group2 = findViewById(R.id.group2);
+        group3 = findViewById(R.id.group3);
 
         creategroup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,10 +48,36 @@ public class Groups extends AppCompatActivity {
                 if (optionsAreVisible){
                     creategroup.setVisibility(View.INVISIBLE);
                     joingroup.setVisibility(View.INVISIBLE);
+                    optionsAreVisible = false;
                 } else {
                     creategroup.setVisibility(View.VISIBLE);
                     joingroup.setVisibility(View.VISIBLE);
+                    optionsAreVisible = true;
                 }
+            }
+        });
+
+        group1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GroupCalendar.class);
+                startActivity(intent);
+            }
+        });
+
+        group2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GroupCalendar.class);
+                startActivity(intent);
+            }
+        });
+
+        group3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GroupCalendar.class);
+                startActivity(intent);
             }
         });
     }
