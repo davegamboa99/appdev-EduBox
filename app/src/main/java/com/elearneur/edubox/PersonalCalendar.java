02ff7f553e9eff3daaa4e5b087ed4e413730bea6
desierto <monchingdesierto49@gmail.com>
2020-11-21 18:27:11 +0800

    pop-up edit view added

--- app/src/main/java/com/elearneur/edubox/PersonalCalendar.java
+++ app/src/main/java/com/elearneur/edubox/PersonalCalendar.java
@@ -4,18 +4,25 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class PersonalCalendar extends AppCompatActivity {
     private Toolbar toolbar;
     private FloatingActionButton addevent;
+    private TextView event001;
+    private Dialog editevent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +32,23 @@ public class PersonalCalendar extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar_calendar);
         setSupportActionBar(toolbar);
         addevent = findViewById(R.id.addevent);
+        event001 = findViewById(R.id.event001);
+        editevent = new Dialog(this);
 
         addevent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), EventAdd.class);
                 startActivity(intent);
+            }
+        });
+
+        event001.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editevent.setContentView(R.layout.event_edit);
+                editevent.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                editevent.show();
             }
         });
     }
